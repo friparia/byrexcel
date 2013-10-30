cb2e312925991d1ba1fd784acbda95925f6cc4d6
friparia <friparia@gmail.com>
2013-10-30 16:22:03 +0800

    fix bug

--- PHPValidator/Rules/PhoneValidator.php
+++ PHPValidator/Rules/PhoneValidator.php
@@ -2,9 +2,10 @@
 class PhoneValidator extends PHPValidator{
     protected function validateAttribute($values,$attribute){
         $value = $values[$attribute];
-        if($value == '18810541532')
+        $pattern="/^((\(\d{2,3}\))|(\d{3}\-))?13\d{9}$/";
+        if(preg_match($pattern, $value))
             return true;
         else
-            return 'PHONE MUST 18810541532';
+            return "INVALID PHONE";
     }
 }
